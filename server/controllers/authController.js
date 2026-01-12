2a76ed46652dca10a423ed9ad8ec6f18e984e363
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-12 23:45:21 +0530

    Update response messages

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,6 +31,8 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000});
 
+        res.json({success: true, message: 'Registration Successfully'});
+
     }catch(error){
         res.json({success: false, message: error.message});
     }
@@ -61,7 +63,7 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000});
 
-        return res.json({success: true, message: 'Login successful'});
+        return res.json({success: true, message: 'Login Successful'});
 
     }catch(error){
         return res.json({success: false, message: error.message});
