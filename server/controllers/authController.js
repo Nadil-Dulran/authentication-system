907d063d429367f4f490e5eee41e888c4f9adf48
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-13 23:13:50 +0530

    Develop method to rest password with OTP and email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -215,3 +215,43 @@ export const sendResetOtp = async (req, res) => {
         res.json({success: false, message: error.message});
     }
 }
+
+// Reset Password using OTP
+export const resetPassword = async (req, res) => {
+    
+    const {email, otp, newPassword} = req.body;
+
+    if(!email || !otp || !newPassword){
+        return res.json({success: false, message: 'Email, OTP and New Password are required'});
+    }
+
+    try{
+
+        const user = await userModel.findOne({ email });
+
+        if(!user){
+            return res.json({success: false, message: 'User not found'});
+        }
+
+        if(user.resetOtp === "" || user.resetOtp !== otp){
+            return res.json({success: false, message: 'Invalid OTP'});
+        }
+
+        if(user.resetOtpExpireAt < Date.now()){
+            return res.json({success: false, message: 'OTP Expired'});
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        
+        user.password = hashedPassword;
+        user.resetOtp = '';
+        user.resetOtpExpireAt = 0;
+
+        await user.save();
+
+        return res.json({success: true, message: 'Password Reset Successfully'});
+
+    }catch(error){
+        res.json({success: false, message: error.message});
+    }
+}
