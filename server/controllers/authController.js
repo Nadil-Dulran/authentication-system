de16f44a6a4c8eeb88b315a440002b0e81b77cb1
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-13 21:49:43 +0530

    Develop script to send welcome mail

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req, res) => {
     const {name, email, password} = req.body;
@@ -30,6 +31,16 @@ export const register = async (req, res) => {
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
 
         res.cookie('token', token, {httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000});
+
+        // Sending Welcome Email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to Our Platform',
+            text: `Hello ${name},\n\nWelcome to our platform! We're excited to have you on board. Your account has been successfully created with the email: ${email}\n\nBest regards,\nNadil Dulran Gamage`
+        }
+        await transporter.sendMail(mailOptions);
+
 
         res.json({success: true, message: 'Registration Successfully'});
 
