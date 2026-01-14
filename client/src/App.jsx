fff2d8f84abb1379eae78b5fbd1f430923a39f09
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-14 21:24:19 +0530

    Update dependencies with Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">
+        Hello
+      </h1>
+    </div>
   )
 }
 
