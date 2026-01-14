1ecc278140055de8091cbd1eb534ccef68189087
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-14 13:26:12 +0530

    Update routes to get user data

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,6 +19,8 @@ app.use(cors({credentials: true}));
 // API Endpoints
 app.get('/', (req, res) => res.send('Api is running...'));
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
