0ab269e8beafde2e54a6eb153cfe1f8aeb7ffca6
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-17 15:06:53 +0530

    Develop frontend of home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,11 @@ import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
         <Navbar/>
         <Header/>
     </div>
   )
 }
-
+//bg-gradient-to-b from-gray-400 to-gray-100 relative overflow-hidden- (Sample background)
 export default Home
