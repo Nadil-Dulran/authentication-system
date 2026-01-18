3c06bbbaa9ac7a2a1b5836d01b720a07d117506d
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-18 21:06:11 +0530

    Develop to get backend user data

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,7 @@
+import axios from "axios"
 import { useState } from "react"
 import { createContext } from "react"
+import { toast } from "react-toastify"
 
 export const AppContent = createContext()
 
@@ -9,10 +11,24 @@ export const AppContextProvider = (props) => {
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
+    const getUserData = async () => {
+        try{
+
+            const {data} = await axios.get(backendUrl + '/api/user/data')
+            data.success ? setUserData(data.userData) : toast.error(data.message)
+
+        }catch(error){
+
+            toast.error(error.message)
+
+        }
+    }
+
     const value = {
         backendUrl,
         isLoggedin, setIsLoggedin,
-        userData, setUserData
+        userData, setUserData,
+        getUserData
     }
 
     return (
