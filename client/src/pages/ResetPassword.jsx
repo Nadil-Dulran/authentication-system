39255c252f45e50010b688e7dd4c1a4bac3a7b48
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-20 22:54:23 +0530

    Develop frontend of reset password

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const ResetPassword = () => {
+
+  const navigate = useNavigate()
   return (
-    <div>
-        <h1>Reset Password</h1>
+    <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-300'>
+        <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 sm:w-32 cursor-pointer'/>
+        
+          {/* enter email to reset password */}
+        <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+          <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset Password</h1>
+                <p className='text-center mb-6 text-indigo-300'>Please Enter registered email to reset password</p>
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                  <img src={assets.mail_icon} alt="" className='w-3 h-3'/>
+                  <input type="email" placeholder='Email' className='bg-transparent outline-none text-white'/>
+                </div>
+        </form>
+        
     </div>
   )
 }
